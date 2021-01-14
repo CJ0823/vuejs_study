77fc3dda9aafe6a08fc10faae3ab58bfa41c3ad6
CJ0823 <changjun0823@gmail.com>
2021-01-14 13:17:09 +0900

    install vue and bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+//BootstrapVue 설치
+Vue.use(BootstrapVue);
+//Optionally install the bootstrapVue icon components plugin
+Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false
 
